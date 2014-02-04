cc31d0db22a69d8aa773fc1ce6c966d66e98ec75
TheBigW <tobiaswenig@yahoo.com>
2014-02-04 21:02:46 +0100

    add usage rbcompat

--- Equalizer.py
+++ Equalizer.py
@@ -1,6 +1,6 @@
 # MyEqualizer.py
 # Copyright (C) 2013 - Tobias Wenig
-#			tobiaswenig@yahoo.com>
+#            tobiaswenig@yahoo.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,50 +15,67 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from gi.repository import Gtk, Gio, Gdk, GdkPixbuf, Gst
+from gi.repository import Gtk
 from config import Config
 from EQBandParams import EQBandParams
-import os, sys, inspect
+#import os, sys, inspect
+
+from ParametricEQ_rb3compat import ActionGroup
+from ParametricEQ_rb3compat import Action
+from ParametricEQ_rb3compat import ApplicationShell
+
+ui_string="""
+<ui> 
+  <menubar name="MenuBar"> 
+    <menu name="ControlMenu" action="Control"> 
+      <placeholder name="PluginPlaceholder">
+        <menuitem name="Parametric Equalizer" action="Equalize"/>
+      </placeholder>
+    </menu>
+  </menubar>
+</ui>
+"""
+
 
 class LabeledEdit:
     def __init__(self, box, text, value):
         label = Gtk.Label( text )
         box.add(label)
         self.entry = Gtk.Entry()
-	self.entry.set_text( value )
-	box.add( self.entry )
+        self.entry.set_text( value )
+        box.add( self.entry )
         
 class AddDialog(Gtk.Dialog):
     params = EQBandParams(0,0,0)
     def __init__(self, parent):
         super(Gtk.Dialog, self).__init__()
-	okBtn = self.add_button(Gtk.STOCK_OK,Gtk.ResponseType.OK)
-	okBtn.connect( "clicked", self.on_ok )    
-	self.set_default_size(150, 100)
+        okBtn = self.add_button(Gtk.STOCK_OK,Gtk.ResponseType.OK)
+        okBtn.connect( "clicked", self.on_ok )    
+        self.set_default_size(150, 100)
         box = self.get_content_area()
         self.bandWidthLE = LabeledEdit( box, "Bandwidth", "10" );
         self.freqLE = LabeledEdit( box, "frequency", "100" );
         self.gainLE = LabeledEdit( box, "Gain", "0" );
-	name_store = Gtk.ListStore(int, str)
-	name_store.append([ 0, EQBandParams.get_string_from_band_type(0)] )
-	name_store.append([ 1, EQBandParams.get_string_from_band_type(1)] )
-	name_store.append([ 2, EQBandParams.get_string_from_band_type(2)] )
-	self.comboType = Gtk.ComboBox.new_with_model_and_entry(name_store)
-	self.comboType.set_entry_text_column(1)
-	self.comboType.set_active(0)
-	box.add(self.comboType)        
+        name_store = Gtk.ListStore(int, str)
+        name_store.append([ 0, EQBandParams.get_string_from_band_type(0)] )
+        name_store.append([ 1, EQBandParams.get_string_from_band_type(1)] )
+        name_store.append([ 2, EQBandParams.get_string_from_band_type(2)] )
+        self.comboType = Gtk.ComboBox.new_with_model_and_entry(name_store)
+        self.comboType.set_entry_text_column(1)
+        self.comboType.set_active(0)
+        box.add(self.comboType)        
         self.show_all()
     def on_ok(self, param):
-	self.params.gain = int(self.gainLE.entry.get_text())
-	self.params.frequency = int(self.freqLE.entry.get_text())
-	self.params.bandwidth = int(self.bandWidthLE.entry.get_text())
-	self.params.bandType = self.comboType.get_active()
-        
+        self.params.gain = int(self.gainLE.entry.get_text())
+        self.params.frequency = int(self.freqLE.entry.get_text())
+        self.params.bandwidth = int(self.bandWidthLE.entry.get_text())
+        self.params.bandType = self.comboType.get_active()
+
 class EQGroupControl(Gtk.VBox):
     def __init__(self, params, parent):
-	super(Gtk.VBox, self).__init__(False)       
-	self.params = EQBandParams( params.frequency, params.bandwidth, params.gain, params.bandType )
-	self.parent = parent
+        super(Gtk.VBox, self).__init__(False)       
+        self.params = EQBandParams( params.frequency, params.bandwidth, params.gain, params.bandType )
+        self.parent = parent
         adjustment = Gtk.Adjustment(0, 0, 100, 5, 10, 0)
         slider = Gtk.VScale()
         slider.set_range( -24, 12 )
@@ -68,150 +85,162 @@ class EQGroupControl(Gtk.VBox):
         slider.set_size_request( 100, 300 )
         slider.connect( "value_changed", self.slider_changed )
         labelFreq = Gtk.Label( "f=" + str(self.params.frequency) + "Hz" )
-        labelBw = Gtk.Label( "w=" + str(self.params.bandwidth) + "Hz" )	
-	labelType = Gtk.Label( EQBandParams.get_string_from_band_type(self.params.bandType) )
+        labelBw = Gtk.Label( "w=" + str(self.params.bandwidth) + "Hz" )    
+        labelType = Gtk.Label( EQBandParams.get_string_from_band_type(self.params.bandType) )
         self.add(slider)
         self.add(labelFreq);
         self.add(labelBw);
-	self.add(labelType)
+        self.add(labelType)
         remBtn = Gtk.Button( "Remove" )
-	remBtn.connect( "clicked", self.on_remove_band )
+        remBtn.connect( "clicked", self.on_remove_band )
         self.add( remBtn )
-	self.show_all()
+        self.show_all()
     def slider_changed(self, hscale):
         print "hscale : ", hscale
-	self.params.gain = hscale.get_value();
+        self.params.gain = hscale.get_value();
         print 'slider changed for ' + str(self.params.frequency) + ' Hz to ' + str(self.params.gain)
-	self.parent.gain_changed()
+        self.parent.gain_changed()
     def on_remove_band(self, param):
-	self.parent.on_remove_band(self)
+        self.parent.on_remove_band(self)
 
 class EQControl(Gtk.Dialog):
     def update_from_preset(self,preset, cfg):
-	self.params = cfg.load(preset)
-	numEqBands = len(self.params)
-	print "numEqBands : ", numEqBands
-	self.set_default_size( numEqBands * 100, 350)
-	self.rebuild_eq_controls()
+        self.params = cfg.load(preset)
+        numEqBands = len(self.params)
+        print "numEqBands : ", numEqBands
+        self.set_default_size( numEqBands * 100, 350)
+        self.rebuild_eq_controls()
     def __init__(self, eq):
-	super(Gtk.Dialog, self).__init__()
-	self.set_deletable(False)	
-	self.eq = eq
-	self.connect( "delete-event", self.on_destroy )
-	self.set_title( "N Bands parametric EQ" )	
-	closeBtn = self.add_button(Gtk.STOCK_CLOSE,Gtk.ResponseType.CLOSE)
-	closeBtn.connect( "clicked", self.on_close )
+        super(Gtk.Dialog, self).__init__()
+        self.set_deletable(False)    
+        self.eq = eq
+        self.connect( "delete-event", self.on_destroy )
+        self.set_title( "N Bands parametric EQ" )    
+        closeBtn = self.add_button(Gtk.STOCK_CLOSE,Gtk.ResponseType.CLOSE)
+        closeBtn.connect( "clicked", self.on_close )
         buttonBox = Gtk.HBox(False)
         addBtn = Gtk.Button( "Add band" )
         addBtn.connect( "clicked", self.add_new_eq_band )
-	buttonBox.add(addBtn);
-	applyBtn = Gtk.Button( "Save" )
+        buttonBox.add(addBtn);
+        applyBtn = Gtk.Button( "Save" )
         applyBtn.connect( "clicked", self.on_apply_settings )
         buttonBox.add(applyBtn)
-	#combo box for presets
-	presetStore = Gtk.ListStore(int, str)
-	cfg = Config()
-	presets = cfg.getAllPresets()	
-	num_presets = len(presets)
-	for i in range(0, num_presets):
-		presetStore.append([ i, presets[i] ] )
-		print "adding preset : ", presets[i]
-	self.comboPresets = Gtk.ComboBox.new_with_model_and_entry(presetStore)
-	self.comboPresets.set_entry_text_column(1)
-	currPreset = cfg.getCurrPreset()
-	currPresetIndex = 0
-	if num_presets > 0:
-		currPresetIndex = presets.index( currPreset )	
-	print "preset index : ", currPresetIndex	
-	self.comboPresets.set_active(currPresetIndex)
-	self.comboPresets.connect( "changed", self.onPresetChanged )
-	buttonBox.add( Gtk.Label( "preset : " ) )
-	buttonBox.add(self.comboPresets)
-	#add a link button to the github for documentation
-	linkButton = Gtk.LinkButton("https://github.com/TheBigW/ParametricEQ/blob/master/README.md", label="HowTo")
-	buttonBox.add(linkButton) 
-	self.vbox.add(buttonBox)
-	self.newHBox = None
-	self.update_from_preset( currPreset, cfg )
+        #combo box for presets
+        presetStore = Gtk.ListStore(int, str)
+        cfg = Config()
+        presets = cfg.getAllPresets()    
+        num_presets = len(presets)
+        for i in range(0, num_presets):
+            presetStore.append([ i, presets[i] ] )
+            print "adding preset : ", presets[i]
+        self.comboPresets = Gtk.ComboBox.new_with_model_and_entry(presetStore)
+        self.comboPresets.set_entry_text_column(1)
+        currPreset = cfg.getCurrPreset()
+        currPresetIndex = 0
+        if num_presets > 0:
+            currPresetIndex = presets.index( currPreset )    
+        print "preset index : ", currPresetIndex    
+        self.comboPresets.set_active(currPresetIndex)
+        self.comboPresets.connect( "changed", self.onPresetChanged )
+        buttonBox.add( Gtk.Label( "preset : " ) )
+        buttonBox.add(self.comboPresets)
+        #add a link button to the github for documentation
+        linkButton = Gtk.LinkButton("https://github.com/TheBigW/ParametricEQ/blob/master/README.md", label="HowTo")
+        buttonBox.add(linkButton) 
+        self.vbox.add(buttonBox)
+        self.newHBox = None
+        self.update_from_preset( currPreset, cfg )
     def onPresetChanged(self, comboPresets):
-	tree_iter = comboPresets.get_active_iter()
-	if tree_iter != None:
+        tree_iter = comboPresets.get_active_iter()
+        if tree_iter != None:
             model = comboPresets.get_model()
             row_id, preset = model[tree_iter][:2]
             print "Selected: id=%d, preset=%s" % (row_id, preset)
-	    cfg = Config()
+            cfg = Config()
             self.update_from_preset(preset, cfg)
     def rebuild_eq_controls(self):
-	numEqBands = len(self.params)
-	if self.newHBox != None:	
-		self.vbox.remove(self.newHBox)	
-		self.newHBox.destroy()
-	self.newHBox = Gtk.HBox(False)
-	for i in range(0,numEqBands):
+        numEqBands = len(self.params)
+        if self.newHBox != None:    
+            self.vbox.remove(self.newHBox)    
+            self.newHBox.destroy()
+        self.newHBox = Gtk.HBox(False)
+        for i in range(0,numEqBands):
             self.newHBox.add(EQGroupControl( self.params[i], self ))
-	self.vbox.add(self.newHBox)
-	self.newHBox.show_all()
-	self.eq.apply_settings(self.params)
+        self.vbox.add(self.newHBox)
+        self.newHBox.show_all()
+        self.eq.apply_settings(self.params)
     def on_destroy(self, widget, data):
-	self.on_close(None)
-	return True
+        self.on_close(None)
+        return True
     def gain_changed(self):
-	self.updateParamList()
-	self.eq.apply_settings( self.params )
+        self.updateParamList()
+        self.eq.apply_settings( self.params )
     def on_apply_settings(self, some_param):
-	self.updateParamList()
-	self.eq.apply_settings( self.params )
-	print "num params to save : ", len(self.params)
-	cfg = Config()
-	tree_iter = self.comboPresets.get_active_iter()
-	preset = ""        
-	if tree_iter != None:
+        self.updateParamList()
+        self.eq.apply_settings( self.params )
+        print "num params to save : ", len(self.params)
+        cfg = Config()
+        tree_iter = self.comboPresets.get_active_iter()
+        preset = ""        
+        if tree_iter != None:
             model = self.comboPresets.get_model()
             row_id, name = model[tree_iter][:2]
             preset = name
         else:
             entry = self.comboPresets.get_child()
-	    preset = entry.get_text()
-	#gconf does not allow spaces -> so we replace with _	
-	preset = preset.replace( " ", "_" )
-	print "curr preset: ", preset
-	cfg.save( self.params, preset )
+        preset = entry.get_text()
+        #gconf does not allow spaces -> so we replace with _    
+        preset = preset.replace( " ", "_" )
+        print "curr preset: ", preset
+        cfg.save( self.params, preset )
     def updateParamList(self):
-	self.params = []
-	eqBandctrls = self.newHBox.get_children()
-	print "children : ", len(eqBandctrls)
-	numBands = len(eqBandctrls)
-	for i in range(0,numBands):
-		self.params.append( eqBandctrls[i].params )
-	print "num bands :", numBands
+        self.params = []
+        eqBandctrls = self.newHBox.get_children()
+        print "children : ", len(eqBandctrls)
+        numBands = len(eqBandctrls)
+        for i in range(0,numBands):
+            self.params.append( eqBandctrls[i].params )
+        print "num bands :", numBands
     def add_new_eq_band(self, param):
-	self.dlg = AddDialog(self)
+        self.dlg = AddDialog(self)
         if self.dlg.run() == Gtk.ResponseType.OK : 
-		self.updateParamList()		
-		self.params.append( self.dlg.params )
-		numBands = len(self.params)		
-		for i in range(0,numBands):
-			print "before sort : Param %f, %f" % (self.params[i].frequency, self.params[i].bandwidth)		
-		self.params.sort()#ascending order for frequency
-		for i in range(0,numBands):
-			print "Param %f, %f" % (self.params[i].frequency, self.params[i].bandwidth) 
-		self.rebuild_eq_controls()
-	self.dlg.destroy()
+            self.updateParamList()        
+            self.params.append( self.dlg.params )
+            numBands = len(self.params)        
+            for i in range(0,numBands):
+                print "before sort : Param %f, %f" % (self.params[i].frequency, self.params[i].bandwidth)        
+            self.params.sort()#ascending order for frequency
+            for i in range(0,numBands):
+                print "Param %f, %f" % (self.params[i].frequency, self.params[i].bandwidth) 
+            self.rebuild_eq_controls()
+        self.dlg.destroy()
     def on_remove_band(self,eqbandCtrl):
-	numParams = len(self.params)
-	param = None
-	for i in range(0,numParams):
-		if eqbandCtrl.params.frequency == self.params[i].frequency:
-			param = self.params[i]
-			break	
-	self.params.remove(param)
-	self.rebuild_eq_controls()
+        numParams = len(self.params)
+        param = None
+        for i in range(0,numParams):
+            if eqbandCtrl.params.frequency == self.params[i].frequency:
+                param = self.params[i]
+                break    
+        self.params.remove(param)
+        self.rebuild_eq_controls()
     def on_close(self, shell):
-	print "closing ui"
-	self.set_visible(False)
-	return True
+        print "closing ui"
+        self.set_visible(False)
+        return True
     def show_ui(self, shell, state):
-	print "showing UI"
+        print "showing UI"
         self.show_all()
         self.present()
-	print "done showing UI"
+        print "done showing UI"
+    def add_ui(self, shell):
+        print "starting add_ui"
+        #self.show_ui(shell,True)
+        #return
+        action_group = ActionGroup(shell, 'ParametricEqualizerActionGroup')
+        action_group.add_action(func=self.show_ui,
+            action_name='Equalize', label=_('_Equalizer'),
+            action_type='app')
+        self._appshell = ApplicationShell(shell)
+        self._appshell.insert_action_group(action_group)
+        self._appshell.add_app_menuitems(ui_string, 'ParametricEqualizerActionGroup')
+        print "add_ui done"
--- ParametricEQ.py
+++ ParametricEQ.py
@@ -1,6 +1,6 @@
 # ParametricEQ.py
 # Copyright (C) 2013 - Tobias Wenig
-#			tobiaswenig@yahoo.com>
+#            tobiaswenig@yahoo.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,125 +16,98 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import os, sys, inspect
-
-from gi.repository import GObject, Gst, Peas, RB, Gtk, Gdk, GdkPixbuf
+from gi.repository import GObject, Gst, Peas
+#from gi.repository import RB
 from Equalizer import EQControl, EQBandParams
 from config import Config
 
-MY_ICON_IMAGE = "MY_ITEM_IMAGE"
+class ParametricEQPlugin (GObject.Object, Peas.Activatable):
+    __gtype_name__ = 'ParametricEQPlugin'
+    object = GObject.property(type=GObject.Object)
 
-class ParametricEQPlugin(GObject.Object, Peas.Activatable):
-	object = GObject.property(type = GObject.Object)
-	def __init__(self):
-		super(ParametricEQPlugin, self).__init__()
+    def __init__(self):
+        super(ParametricEQPlugin, self).__init__()
+        print "init done"
 
-	def do_activate(self):
-		self.shell = self.object
-		self.shell_player = self.shell.props.shell_player
-		self.player = self.shell_player.props.player
-		self.eq = Gst.ElementFactory.make('equalizer-nbands', 'MyEQ')	
-		conf = Config()		
-		params = conf.load(None)
-		if len(params) == 0:
-			#no default config for active preset - create a 10 Band default EQ to get started
-			params =  [	EQBandParams(29, 20, 0),
-					EQBandParams(59, 30, 0),
-					EQBandParams(119, 60, 0),
-					EQBandParams(237, 118, 0),
-					EQBandParams(474, 237, 0),
-					EQBandParams(947, 473, 0),
-					EQBandParams(1889, 942, 0),
-					EQBandParams(3770, 1881, 0),
-					EQBandParams(7523, 3753, 0),
-					EQBandParams(15011, 7488, 0) ]
-			conf.save(params,"default_10_band")
-		self.eqDlg = EQControl(self)
-		self.add_ui( self, self.shell )
-		self.filterSet = False
-		self.apply_settings(params)
-		self.psc_id = self.shell_player.connect('playing-song-changed', self.playing_song_changed)
-	
-	def add_ui(self, plugin, shell):
-		icon_factory = Gtk.IconFactory()
-		filename = plugin.find_file("ParametricEQ.svg")
-		print "svg: ", filename		
-		picBuff = GdkPixbuf.Pixbuf.new_from_file(filename)
-		#print inspect.getdoc( picBuff )
-		icon_factory.add( MY_ICON_IMAGE, Gtk.IconSet( picBuff ) )
-		icon_factory.add_default()
+    def set_filter(self):
+        try:
+            if self.filterSet:
+                return
+            print 'adding filter'
+            self.player.add_filter(self.eq)
+            self.filterSet = True
+            print 'done setting filter'
+        except Exception as inst:
+            print 'unexpected exception',  sys.exc_info()[0], type(inst), inst  
+            pass
 
-		action = Gtk.Action ('Equalize', 
-				_('_ParametricEQ'), 
-				_('N Band Equalizer'),
-				MY_ICON_IMAGE)
-		action.connect ('activate', self.eqDlg.show_ui, shell)
-		action_group = Gtk.ActionGroup ('EqualizerActionGroup')
-		action_group.add_action (action)
-		ui_manager = shell.props.ui_manager
-		ui_manager.insert_action_group (action_group)
-		ui_manager.add_ui_from_file(plugin.find_file("equalizer-ui.xml"))
-	
-	def set_filter(self):
-		try:
-			if self.filterSet:
-				return			
-			print 'adding filter'
-			self.player.add_filter(self.eq)
-			self.filterSet = True
-			print 'done setting filter'
-		except Exception as inst:
-			print 'unexpected exception',  sys.exc_info()[0], type(inst), inst  
-			pass
+    def do_deactivate(self):
+        print 'entering do_deactivate'
+        try:        
+            self.player.remove_filter(self.eq)
+            print 'filter disabled'    
+        except:
+            pass
+                    
+        del self.shell_player
+        del self.shell
+        del self.eq
 
-	def do_deactivate(self):
-		print 'entering do_deactivate'
-		self.shell_player.disconnect(self.psc_id)
-		
-		try:		
-			self.player.remove_filter(self.eq)
-			print 'filter disabled'	
-		except:
-			pass
-					
-		del self.shell_player
-		del self.shell
-		del self.eq
+    def apply_settings(self,params):
+        return
+        numEQBands = len( params )
+        result = False
+        print "num-bands : ", numEQBands
+        if numEQBands > 0:
+            print "got eq bands"
+            self.eq.set_property('num-bands', numEQBands)
+            for i in range(0,numEQBands):
+                band = self.eq.get_child_by_index(i)
+                #print inspect.getdoc( band.props.freq )
+                band.props.freq = params[i].frequency
+                print 'band.props.freq', band.props.freq
+                band.props.bandwidth = params[i].bandwidth
+                print 'band.props.bandwidth', band.props.bandwidth
+                band.props.gain = params[i].gain
+                print 'band.props.gain', band.props.gain
+                band.props.type = params[i].bandType
+            result = True
+        if True == result:
+            self.set_filter()
 
-	def playing_song_changed(self, sp, entry):
-		if entry == None:
-			return
-		genre = entry.get_string(RB.RhythmDBPropType.GENRE)
-		print "genre : " + str(genre)
-		print entry.get_string(RB.RhythmDBPropType.ALBUM)
-		print entry.get_string(RB.RhythmDBPropType.TITLE)
-	def find_file(self, filename):
-		info = self.plugin_info
-		data_dir = info.get_data_dir()
-		path = os.path.join(data_dir, filename)
-		
-		if os.path.exists(path):
-			return path
+    def do_activate(self):
+        self.shell = self.object
+        self.shell_player = self.shell.props.shell_player
+        self.player = self.shell_player.props.player
+        self.eq = Gst.ElementFactory.make('equalizer-nbands', 'MyEQ')
+        conf = Config()
+        params = conf.load(None)
+        if len(params) == 0:
+            #no default config for active preset - create a 10 Band default EQ to get started
+            params =  [EQBandParams(29, 20, 0),
+            EQBandParams(59, 30, 0),
+            EQBandParams(119, 60, 0),
+            EQBandParams(237, 118, 0),
+            EQBandParams(474, 237, 0),
+            EQBandParams(947, 473, 0),
+            EQBandParams(1889, 942, 0),
+            EQBandParams(3770, 1881, 0),
+            EQBandParams(7523, 3753, 0),
+            EQBandParams(15011, 7488, 0)]
+            conf.save(params,"default_10_band")
+        self.eqDlg = EQControl(self)
+        self.eqDlg.add_ui( self.shell )
+        self.filterSet = False
+        self.apply_settings(params)
+        print "do_activate done"
 
-		return RB.file(filename)
-	
-	def apply_settings(self,params):
-		numEQBands = len( params )
-		result = False
-		print "num-bands : ", numEQBands
-		if numEQBands > 0:
-			print "got eq bands"
-			self.eq.set_property('num-bands', numEQBands)
-			for i in range(0,numEQBands):
-				band = self.eq.get_child_by_index(i)
-				#print inspect.getdoc( band.props.freq )
-				band.props.freq = params[i].frequency
-				print 'band.props.freq', band.props.freq
-				band.props.bandwidth = params[i].bandwidth
-				print 'band.props.bandwidth', band.props.bandwidth
-				band.props.gain = params[i].gain
-				print 'band.props.gain', band.props.gain
-				band.props.type = params[i].bandType
-			result = True
-		if True == result:
-			self.set_filter()
-
+'''
+    def find_file(self, filename):
+        info = self.plugin_info
+        data_dir = info.get_data_dir()
+        path = os.path.join(data_dir, filename)
+        
+        if os.path.exists(path):
+            return path
+        return RB.file(filename)
+'''
